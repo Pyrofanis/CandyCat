0172c2b8255a32705ae539fb009d23a646b0930c
nasos karachalios stagkas <nasos.karachalios.stagas@gmail.com>
2021-07-16 10:23:57 +0300

    need to add the boundaries at swap locs

--- Assets/Scripts/Restart/TilesScripts/Logic/AvailableLocs.cs
+++ Assets/Scripts/Restart/TilesScripts/Logic/AvailableLocs.cs
@@ -49,15 +49,68 @@ public class AvailableLocs : MonoBehaviour
     }
     private void FindSwapablePoints()
     {
-        for (int i = 0; i < tilesData.locsOfSameTypeTiles.Count; i++)
+            //adds all available moveable locations
+            AddAvailableTilesToMove(tilesData.currentMap, 1, 0, tilesData.SwapableLocs);
+            AddAvailableTilesToMove(tilesData.currentMap, -1, 0, tilesData.SwapableLocs);
+            AddAvailableTilesToMove(tilesData.currentMap, 0, 1, tilesData.SwapableLocs);
+            AddAvailableTilesToMove(tilesData.currentMap, 0, -1, tilesData.SwapableLocs);
+        //removes not symbiotic choices
+        foreach(Vector2 vector in tilesData.SwapableLocs)
         {
-            CheckIfEqual(tilesData.currentMap, 1, 0, tilesData.SwapableLocs);
-            CheckIfEqual(tilesData.currentMap, -1, 0, tilesData.SwapableLocs);
-            CheckIfEqual(tilesData.currentMap, 0, 1, tilesData.SwapableLocs);
-            CheckIfEqual(tilesData.currentMap, 0, -1, tilesData.SwapableLocs);
+            Vector2Int currentVector = Vector2Int.CeilToInt(vector);
+            //check by rows and collumns for pairs of two and removes if not eg xxo
+            //x check pos
+            CheckIfMachingCollumnOrRowIsCreated(1, 0, 1, tilesData.currentMap, currentVector);
+            //x check neg
+            CheckIfMachingCollumnOrRowIsCreated(1, 0, -1, tilesData.currentMap, currentVector);
+            //y check pos
+            CheckIfMachingCollumnOrRowIsCreated(0, 1, 1, tilesData.currentMap, currentVector);
+            //y check neg
+            CheckIfMachingCollumnOrRowIsCreated(0, 1, -1, tilesData.currentMap, currentVector);
+            //checks if a midsection is created ej xox by rows and collumns
+            //x check
+            MidPosCheckCollumnRow(1, 0, tilesData.currentMap, currentVector);
+            //y check
+            MidPosCheckCollumnRow(0, 1, tilesData.currentMap, currentVector);
         }
     }
-    private void CheckIfEqual(BoxesData.TypeNPrefab[,] map, int indexX, int indexY,List<Vector2> swapableLocations)
+    private void CheckIfMachingCollumnOrRowIsCreated(int indexX,int indexY,int negativeOrPositve,BoxesData.TypeNPrefab[,] map,Vector2Int swapableCordsVector)
+    {
+        int offsetXCoord1= swapableCordsVector.x + indexX * negativeOrPositve;
+        int offsetYCoord1= swapableCordsVector.y + indexY * negativeOrPositve;
+        BoxesData.TypeNPrefab coord1Obj=map[offsetXCoord1, offsetYCoord1];
+        int offsetXCoord2=offsetXCoord1+1*indexX*negativeOrPositve;
+        int offsetYCoord2= offsetYCoord1 + 1 * indexY * negativeOrPositve;
+        BoxesData.TypeNPrefab coord2Obj= map[offsetXCoord2,offsetYCoord2];
+
+        Vector2 coord1 = new Vector2(coord1Obj.x, coord1Obj.y);
+        Vector2 coord2 = new Vector2(coord2Obj.x, coord2Obj.y);
+        if (!tilesData.locsOfSameTypeTiles.Contains(coord1)|| !tilesData.locsOfSameTypeTiles.Contains(coord2))
+        {
+            tilesData.SwapableLocs.Remove(swapableCordsVector);
+            tilesData.SwapableLocs.TrimExcess();
+        }
+        
+    }
+    private void MidPosCheckCollumnRow(int indexX,int indexY, BoxesData.TypeNPrefab[,] map, Vector2Int swapableCordsVector)
+    {
+        int offsetXCoord1 = swapableCordsVector.x + indexX ;
+        int offsetYCoord1 = swapableCordsVector.y + indexY ;
+        BoxesData.TypeNPrefab coord1Obj = map[offsetXCoord1, offsetYCoord1];
+        int offsetXCoord2 = offsetXCoord1 -  indexX ;
+        int offsetYCoord2 = offsetYCoord1 -  indexY ;
+        BoxesData.TypeNPrefab coord2Obj = map[offsetXCoord2, offsetYCoord2];
+        Vector2 coord1 = new Vector2(coord1Obj.x, coord1Obj.y);
+        Vector2 coord2 = new Vector2(coord2Obj.x, coord2Obj.y);
+        if (!tilesData.locsOfSameTypeTiles.Contains(coord1) || !tilesData.locsOfSameTypeTiles.Contains(coord2))
+        {
+            tilesData.SwapableLocs.Remove(swapableCordsVector);
+            tilesData.SwapableLocs.TrimExcess();
+        }
+
+    }
+
+    private void AddAvailableTilesToMove(BoxesData.TypeNPrefab[,] map, int indexX, int indexY,List<Vector2> swapableLocations)
     {
         BoxesData.TypeNPrefab currentTileToAdd = map[tilesData.thisType.x + indexX, tilesData.thisType.y + indexY];
         Vector2 coords = new Vector2(currentTileToAdd.x, currentTileToAdd.y);
