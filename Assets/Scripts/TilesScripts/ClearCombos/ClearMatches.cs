4e6ffa9b36730ba9fa5411c11c81feece1b9733d
nasos karachalios stagkas <nasos.karachalios.stagas@gmail.com>
2021-08-09 03:27:59 +0300

    StartedCleatMatches Script

---
+++ Assets/Scripts/TilesScripts/ClearCombos/ClearMatches.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearMatches : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static void ClearCombo(List<BoxesData.TypeNPrefab> currentList,BoxesData.TypeNPrefab currentTipe)
+    {
+        if (currentList.Count >= 2)
+        {
+            //for (int i = 0; i < currentList.Count; i++)
+            //{
+            //    currentList[i].boxType = BoxesData.BoxTypes.none;
+            //}
+        }
+
+    }
+}
